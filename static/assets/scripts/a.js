2290fd9a9775d590b519300790f31c44a68f3427
Hello! I’m proxyproxyproxy <159737858+proxyproxyproxy-scratch@users.noreply.github.com>
2024-11-08 22:31:16 +0900

    Update a.js

--- static/assets/scripts/a.js
+++ static/assets/scripts/a.js
@@ -1,9 +1,8 @@
 let appInd
 
 let appInd;
-const g = window.location.pathname === "/up";
-const a = window.location.pathname === "/yz";
-const c = window.location.pathname === "/gt";
+const g = window.location.pathname === "/gay";
+const a = window.location.pathname === "/aps";
 
 function saveToLocal(path) {
   sessionStorage.setItem('GoUrl', path)
